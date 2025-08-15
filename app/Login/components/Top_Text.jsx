7e992e9eee18dd4939e4dca5567a1b40871e9d74
Your Name <fawzybb123@gmail.com>
2025-08-16 02:24:31 +0300

    res in Top_Text

--- app/Login/components/Top_Text.jsx
+++ app/Login/components/Top_Text.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Top_Text() {
   return (
     <div className="font-normal">
-      <p className="text-3xl sm:text-5xl text-[#1A1A1E] h-[66px]">
+      <p className="text-4xl sm:text-5xl text-[#1A1A1E] h-[66px]">
         Welcome back
       </p>
       <p className="text-[#62626B] h-14 mb-5">
